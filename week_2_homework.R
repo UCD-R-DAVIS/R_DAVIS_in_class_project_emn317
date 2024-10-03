6897cc1f3ef52d55b63e16f5651143c7f041f3f3
Ethan Nichols <max@campus-103-138.ucdavis.edu>
2024-10-03 14:01:31 -0700

    week 2 hw

---
+++ week_2_homework.R
@@ -0,0 +1,57 @@
+1+100
+1 + 1
+1+1
+?exp
+x=2
+x=1
+numDocuments
+log(numDocuments)
+log(x)
+log(5)
+log(5, base = 10)
+log(5, base = e)
+log(5, base = exp(1))
+# One billion
+## one billion
+1e9
+elephant1_kg <- 3492
+elephant1_lb <- 7757
+elephant1_kg > elephant1_lb
+kg = lb
+getwd
+getwd
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+
+
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+is.na(hw2)
+na.omit(hw2)
+hw2
+na.omit(hw2)
+complete.cases(hw2)
+hw2[!is.na(hw2)]
+hw2[complete.cases(hw2)]
+prob1 <- hw2[!is.na(hw2)]
+prob1
+prob1 * 3
+times3 <- (prob1*3)
+time3
+times3
+times3 + 10
+plus10 <- (times3 + 10)
+plus10
+
+prob1 <- hw2[!is.na(hw2)]
+prob1 <- prob1[prob1 >14 & prob1 < 38]
+times3 <- prob1 * 3
+plus10 <- times3 + 10 
+final <- plus10[c(TRUE, FALSE)]
+final
